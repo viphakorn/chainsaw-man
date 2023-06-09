64ff657ce50b9317bbc3fc203e6f5fb707872a97
viphakorn <viphakone585@gmail.com>
2023-06-09 16:34:27 +0700

    test Image

---
+++ export-images.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next-export-optimize-images').Config}
+ */
+const config = {
+  outDir: "dist",
+  convertFormat: [["png", "webp"]],
+};
+
+module.exports = config;
---
+++ imageLoader.js
@@ -0,0 +1,5 @@
+// import { ImageLoaderProps } from "next/image";
+
+export default function ImageLoader({ src }) {
+  return src;
+}
